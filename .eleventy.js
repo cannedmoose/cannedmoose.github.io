f4e156f426a8ed1372e2eb530787e24a1dea3bea
Callan McNamara <callan.mcnamara@gmail.com>
2022-01-17 15:37:49 +1100

    Add Services File

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,19 @@
 //const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const yaml = require("js-yaml");
+const fs = require('fs');
 
 module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
 
   // Merge data instead of overriding
-  //eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.setDataDeepMerge(true);
 
-  // Syntax Highlighting for Code blocks
-  //eleventyConfig.addPlugin(syntaxHighlight);
+  // To Support .yaml Extension in _data
+  // You may remove this if you can use JSON
+  eleventyConfig.addDataExtension("yaml", (contents) =>
+    yaml.safeLoad(contents)
+  );
 
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
@@ -23,6 +28,11 @@ module.exports = function (eleventyConfig) {
   const mdRender = new MarkdownIt();
   eleventyConfig.addFilter("markdownit", function(rawString) {
     return mdRender.render(rawString);
+  });
+
+  eleventyConfig.addFilter("embed", function(rawString) {
+    const data = fs.readFileSync("src" + rawString, 'utf8');
+    return data;
   });
 
   eleventyConfig.addPairedNunjucksShortcode("comment", function(_) {return ""});
